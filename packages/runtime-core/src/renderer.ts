9d6307bae12df2d47a1c9b1ce5a379ae2012fba7
高艳兵 <yanbing.gao@aliyun.com>
2026-02-27 15:52:39 +0800

    feat: renderer

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1,3 +1,11 @@
 export function createRenderer(options) {
   console.log(options);
+
+  const render = (vNode: any, container: Element) => {
+    console.log(vNode, container);
+  };
+
+  return {
+    render,
+  };
 }
--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,5 +1,6 @@
 import { nodeOps } from './nodeOps';
 import { patchProp } from './patchProp';
+import { createRenderer } from '@vue/runtime-core';
 
 /**
  * `runtime-dom` 的渲染器配置项。
@@ -11,10 +12,13 @@ import { patchProp } from './patchProp';
  * - **nodeOps**：节点创建/插入/删除等 DOM 操作
  * - **patchProp**：更新元素属性、样式、事件、特性
  */
-const renderOptions = {
-  patchProp,
-  ...nodeOps,
-};
+const renderOptions = { patchProp, ...nodeOps };
+
+const renderer = createRenderer(renderOptions);
+
+export function render(vNode: any, container: Element) {
+  renderer.render(vNode, container);
+}
 
 export * from '@vue/runtime-core';
 
