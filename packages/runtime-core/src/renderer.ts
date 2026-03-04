114a25dd09b2dd78313e69c83d0bd5e0c9122407
高艳兵 <yanbing.gao@aliyun.com>
2026-03-04 15:16:31 +0800

    feat: 初始化基本结构

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -5,7 +5,20 @@
  * @returns 包含 render 方法的渲染器对象
  */
 export function createRenderer(options) {
-  console.log(options);
+  const unmount = vnode => {
+    console.log(vnode);
+  };
+
+  /**
+   * 更新和挂载的入口
+   * @param n1 老节点，如果有则需要和n2做diff；如果没有则直接挂载n2
+   * @param n2 新节点
+   * @param container 要挂载的容器
+   */
+  const patch = (n1, n2, container) => {
+    // 如果老节点和新节点是同一个节点，则直接返回
+    if (n1 === n2) return;
+  };
 
   /**
    * 把 VNode 渲染到容器中。
@@ -13,8 +26,21 @@ export function createRenderer(options) {
    * @param vNode 虚拟节点
    * @param container 挂载容器
    */
-  const render = (vNode: any, container: Element) => {
-    console.log(vNode, container);
+  const render = (vNode: any, container: any) => {
+    // 分3步：挂载、更新、卸载
+
+    if (vNode === null) {
+      if (container._vnode) {
+        // 卸载老节点
+        unmount(container._vnode);
+      }
+      return;
+    } else {
+      // 挂载新节点
+      patch(container._vnode || null, vNode, container);
+    }
+
+    container._vnode = vNode;
   };
 
   return {
