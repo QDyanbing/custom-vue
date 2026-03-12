77616cb7d17d276906d0055d7e3584d6d868c97a
高艳兵 <yanbing.gao@aliyun.com>
2026-03-12 14:41:12 +0800

    feat: 处理 $forceUpdate

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -69,6 +69,9 @@ const publicPropertiesMap = {
   $nextTick: i => {
     return nextTick.bind(i);
   },
+  $forceUpdate: i => {
+    return () => i.update();
+  },
 };
 
 const publicInstanceProxyHandlers = {
