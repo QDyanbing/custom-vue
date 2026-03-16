8cde116f884c48903ff8537751326a49a68ab52d
高艳兵 <yanbing.gao@aliyun.com>
2026-03-16 16:09:01 +0800

    feat: 子组件的实例

--- packages/runtime-core/src/renderTemplateRef.ts
+++ packages/runtime-core/src/renderTemplateRef.ts
@@ -1,7 +1,16 @@
 import { isRef } from '@vue/reactivity';
+import { ShapeFlags } from '@vue/shared';
 
 export function setRef(ref, vnode) {
+  const { shapeFlag } = vnode;
+
   if (isRef(ref)) {
-    ref.value = vnode.el;
+    if (shapeFlag & ShapeFlags.COMPONENT) {
+      // vnode 是一个组件类型
+      ref.value = vnode.component.exposed;
+    } else {
+      // vnode 是一个 DOM 元素类型
+      ref.value = vnode.el;
+    }
   }
 }
