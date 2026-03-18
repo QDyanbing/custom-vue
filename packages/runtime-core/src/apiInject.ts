95295ae65b49ffd47144dd6a503b964c09337eb3
高艳兵 <yanbing.gao@aliyun.com>
2026-03-18 15:30:36 +0800

    feat: 优化，不是每次都要创建对象

--- packages/runtime-core/src/apiInject.ts
+++ packages/runtime-core/src/apiInject.ts
@@ -1,8 +1,20 @@
 import { getCurrentInstance } from './component';
 
 export const provide = (key: string, value: any) => {
+  // 首次调用的时候，instance.provides 就是 parent.provides
   const instance = getCurrentInstance();
-  const { provides } = instance;
+  // 父组件的 provides
+  const parentProvides = instance.parent ? instance.parent?.provides : instance.appContext.provides;
+  // 自己的 provides
+  let provides = instance.provides;
+
+  if (parentProvides === provides) {
+    // 在此之前，我是没打算给后代组件留有遗产的，我自己的钱都花光了；
+    // 但是中了彩票，这时候就有点零花钱了，要不就留给后端一点吧；
+    instance.provides = Object.create(parentProvides);
+    provides = instance.provides;
+  }
+
   // 设置属性到当前组件的provides中
   provides[key] = value;
 };
