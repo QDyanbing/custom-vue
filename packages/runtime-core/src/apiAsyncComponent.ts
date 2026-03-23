4052b87d9e9606422f6cd5c368d792306b68f27d
高艳兵 <yanbing.gao@aliyun.com>
2026-03-23 14:31:12 +0800

    feat: 更多的加载参数

--- packages/runtime-core/src/apiAsyncComponent.ts
+++ packages/runtime-core/src/apiAsyncComponent.ts
@@ -1,21 +1,38 @@
 import { ref } from '@vue/reactivity';
 import { h } from './h';
+import { isFunction } from '@vue/shared';
 
-export function defineAsyncComponent(loader) {
+export function defineAsyncComponent(options: any) {
+  if (isFunction(options)) {
+    // 如果传入的是函数，则认为是一个异步组件的加载函数，需要包装成对象
+    options = { loader: options };
+  }
+
+  const defaultComponent = () => h('span', null, '');
+
+  const {
+    loader,
+    loadingComponent = defaultComponent,
+    errorComponent = defaultComponent,
+  } = options;
+
   return {
     setup() {
-      const component = ref(() => {
-        return h('span', null, '');
-      });
+      const component = ref(loadingComponent);
 
-      loader().then(comp => {
-        if (comp && comp[Symbol.toStringTag] === 'Module') {
-          comp = comp.default;
-        }
+      loader().then(
+        comp => {
+          if (comp && comp[Symbol.toStringTag] === 'Module') {
+            comp = comp.default;
+          }
 
-        // 组件加载完成，更新组件
-        component.value = comp;
-      });
+          // 组件加载完成，更新组件
+          component.value = comp;
+        },
+        error => {
+          component.value = errorComponent;
+        },
+      );
 
       return () => {
         return h(component.value);
