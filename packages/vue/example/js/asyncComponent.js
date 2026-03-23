c3609350fbc5500ba7c839a08e4e2d3fac1505b8
高艳兵 <yanbing.gao@aliyun.com>
2026-03-23 13:59:13 +0800

    feat: 异步组件演示demo

---
+++ packages/vue/example/js/asyncComponent.js
@@ -0,0 +1,8 @@
+import { h } from '/node_modules/vue/dist/vue.esm-browser.js';
+// import { h } from '../dist/vue.esm.js';
+
+export default {
+  render() {
+    return h('div', 'hello world');
+  },
+};
