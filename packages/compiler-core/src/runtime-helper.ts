271a8ccfd504eac4caa1e5c5774be96496a04dab
高艳兵 <yanbing.gao@aliyun.com>
2026-04-03 10:09:06 +0800

    feat: helper

--- packages/compiler-core/src/runtime-helper.ts
+++ packages/compiler-core/src/runtime-helper.ts
@@ -3,6 +3,9 @@
  */
 export const TO_DISPLAY_STRING = Symbol('toDisplayString');
 
+export const CREATE_TEXT = Symbol('createText');
+
 export const helperMap = {
   [TO_DISPLAY_STRING]: 'toDisplayString',
+  [CREATE_TEXT]: 'createText',
 };
